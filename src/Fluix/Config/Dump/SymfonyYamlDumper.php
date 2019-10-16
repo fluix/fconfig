2c16edf97056470ee0118e4f98a6c911e6a691ba
Vladimir Melnik <melnik.wladimir@gmail.com>
2019-10-16 15:32:18 +0300

    SymfonyYamlDumper: suport not only scalar options

--- src/Fluix/Config/Dump/SymfonyYamlDumper.php
+++ src/Fluix/Config/Dump/SymfonyYamlDumper.php
@@ -6,24 +6,14 @@ namespace Fluix\Config\Dump;
 
 use Fluix\Config\Dumper;
 use Fluix\Config\File;
+use Symfony\Component\Yaml\Yaml;
 
 final class SymfonyYamlDumper implements Dumper
 {
     public function dump(File $file, array $values): void
     {
-        ob_start();
-        echo "parameters:\n";
-        foreach ($values as $k => $v) {
-            $val = "'{$v}'";
-            if (is_bool($v)) {
-                $val = $v ? 'true' : 'false';
-            } elseif (is_numeric($v)) {
-                $val = $v;
-            }
-            echo "    {$k}: {$val}\n";
-        }
-        
-        $file->write(ob_get_clean());
+        $string = Yaml::dump(["parameters" => $values], 2, 4, Yaml::DUMP_EXCEPTION_ON_INVALID_TYPE);
+        $file->write($string);
     }
     
     public function supports(Format $format): bool
